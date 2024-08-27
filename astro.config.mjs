532b9a3e7d228d87a4d97408c3b4ba8a8eee7c13
marcelo <marceloacampora@gmail.com>
2024-08-27 20:44:30 -0300

    added action

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://chelo67.gitlab.io',
     base: '/astro-api/',
+    outDir: 'public',
+  publicDir: 'static',
 });
