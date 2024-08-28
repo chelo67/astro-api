2b27a593f37301b7530f7d67e5f9b3713a68ffee
marcelo <marceloacampora@gmail.com>
2024-08-27 21:02:55 -0300

    arreglo dir

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://chelo67.gitlab.io',
+    site: 'https://chelo67.github.io',
     base: '/astro-api/',
     outDir: 'public',
   publicDir: 'static',
